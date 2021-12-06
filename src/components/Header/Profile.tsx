bb55ab4a78ea9e34910382fa7586a5f86ec3d45a
liviabelirocha <liviabelirocha@outlook.com>
2021-12-06 09:57:42 -0300

    feat: responsive navbar

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Lívia Belizário</Text>
-        <Text color="gray.300" fontSize="small">
-          liviabelirocha@outlook.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Lívia Belizário</Text>
+          <Text color="gray.300" fontSize="small">
+            liviabelirocha@outlook.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar
         size="md"
