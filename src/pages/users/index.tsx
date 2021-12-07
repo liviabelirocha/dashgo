bd3d2ea15e7782969caf35d1a769e30ff063e368
liviabelirocha <liviabelirocha@outlook.com>
2021-12-07 13:20:45 -0300

    feat: adding devtools

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -24,25 +24,31 @@ import { Pagination } from "../../components/Pagination";
 import Link from "next/link";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
+  const { data, isLoading, error } = useQuery(
+    "users",
+    async () => {
+      const response = await fetch("http://localhost:3000/api/users");
+      const data = await response.json();
 
-    const users = data.users.map((user) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
-          day: "2-digit",
-          month: "long",
-          year: "numeric",
-        }),
-      };
-    });
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          }),
+        };
+      });
 
-    return users;
-  });
+      return users;
+    },
+    {
+      staleTime: 1000 * 60 * 10, // 10 minutes
+    }
+  );
 
   const isWideVersion = useBreakpointValue({
     base: false,
